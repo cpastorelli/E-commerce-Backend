7bd8d069c9657fd44521dfa730ff77afbf713a42
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2022-02-09 18:41:53 -0800

    filled in the basic fields for Product. Need to add Validation and default values.

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,27 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING
+    },
+    price: {
+      type: DataTypes.DECIMAL
+      //need to add validation here for price
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      //Default value (10)
+      //need to add validation here for price
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // references category id
+    }
   },
   {
     sequelize,
