a964b7a28d121b3591647527b623702902cc1cb4
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2022-02-09 18:48:42 -0800

    filled in Tag columns from Models

--- models/Category.js
+++ models/Category.js
@@ -7,7 +7,7 @@ class Category extends Model {}
 Category.init(
   {
     id: {
-      type:DataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true
     },
