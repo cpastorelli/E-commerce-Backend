2e3b3dd2774848b4976b1aa1d2c168d5ffd76115
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2022-02-11 17:04:28 -0800

    Began filling in the get routes and delete routes for category, product, and tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,19 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+
+  }).then((categoryData) => {
+    res.json(categoryData);
+  }).catch((err) => res.json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id).then((categoryData) => {
+    res.json(categoryData);
+  }).catch((err) => res.json(err));
 });
 
 router.post('/', (req, res) => {
@@ -23,6 +31,12 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+
+  }).then((deletedCategory) => {
+    res.json(deletedCategoy);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,20 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+
+  }).then((tagData) => {
+    res.json(tagData);
+  }).catch((err) => res.json(err));
+
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id).then((tagData) => {
+    res.json(tagData);
+  })
 });
 
 router.post('/', (req, res) => {
@@ -23,6 +32,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+
+  }).then((deletedTag) => {
+    res.json(deletedTag);
+  }).catch((err) => res.json(err));
 });
 
 module.exports = router;
